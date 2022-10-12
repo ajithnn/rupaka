2a0b4eefd7baddd9284c852ea4b2f9bdaf56507b
ajithnn <ajithnn20@gmail.com>
2022-10-12 18:29:02 +0530

    move to cabal

--- run-examples.sh
+++ run-examples.sh
@@ -3,7 +3,7 @@
 val=0
 while [ $val -le 6 ]
 do
-	cmd=$(echo "stack exec rupaka -- -o examples/outputs-$val.json -c examples/example-$val.cfg -v examples/example-$val.vld -s")
+	cmd=$(echo "cabal v2-run --verbose=0 rupaka -- -o examples/outputs-$val.json -c examples/example-$val.cfg -v examples/example-$val.vld -s")
 	echo "Running: $cmd"
 	$cmd
 	val=$(( $val + 1 ))
